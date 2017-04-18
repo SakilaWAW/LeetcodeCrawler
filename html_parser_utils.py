1f7fdb7884c149afa0be932323d2132716a36d9e
STG <pccp0909@sina.com>
2017-04-18 15:17:33 +0800

    更新html转义字符表

--- html_parser_utils.py
+++ html_parser_utils.py
@@ -9,7 +9,7 @@ class HtmlParserUtils:
                      r"\u003B": ";", r"\u003C": "<", r"\u0026": "&", r"\u0027": "'",
                      r"\u002D": "-", r"\u003E": ">", r"\u0022": "\"", r"\u005C": "\\"}
     # HTML代码中的转义字符
-    HTML_ESCAPE_MAP = {r"a": ""}
+    HTML_ESCAPE_MAP = {'&quot;': '"', '&amp;': '&', '&lt;': '<', '&gt;': '>', '&nbsp;': ' '}
 
     def unescape_html(self, text):
         for key in self.HTML_ESCAPE_MAP:
